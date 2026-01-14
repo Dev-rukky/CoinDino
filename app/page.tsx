959bfb1c0f12502be69da32f9d9bdb73bef46829
Rukky <abrukevwe@gmail.com>
2026-01-14 10:24:06 +0100

    daily dose

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
+import DataTable from '@/components/DataTable'
+import Image from 'next/image'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      
-    </div>
+    <main className='main-container'>
+      <section className='home-grid'>
+        <div id='crypto-overview'>
+          <div className='header pt-2'>
+            <Image src='https://assets.coingecko.com/coins/images/1/large/bitcoin.png' alt='' width={56} height={56} />
+            <div className='info'>
+              <p>Bitcoin / BTC</p>
+              <h1>$93,200.00</h1>
+            </div>
+          </div>
+        </div>
+        <p>Trending Crypto</p>
+        <DataTable columns={[]} />
+      </section>
+      <section className='w-full mt-7 space-y-4 '>
+          <p>Categories</p>
+        </section>
+    </main>
   )
 }
 
